7e5401baf082cc6b1e67bc473594946c27f1fe50
Sergey Podguzov <spo@virtoway.com>
2020-10-02 23:55:50 +0500

    VME-912: NewsSubscriberController was added. Using this controller on blog subscribe form and form in footer in main layouts.

---
+++ theme/assets/scripts/angular/news.js
@@ -0,0 +1,43 @@
+var storefrontApp = angular.module('storefrontApp');
+
+storefrontApp.service('newsSubscriberService', ['$http', '$cookies', '$q', function ($http, $cookies, $q) {
+    return {
+        subscribe: function (form) {
+            var deferred = $q.defer();
+
+            var formData = {
+                formId: event.target.form.id,
+                ip: $cookies.get('current_ip'),
+                email: form['Index[Email]'].$viewValue
+            };
+            $http({
+                method: 'POST',
+                url: `/${shopId}/${cultureName}/call`,
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'X-XSRF-TOKEN': $cookies.get('XSRF-TOKEN'),
+                    service: 'GateLA'
+                },
+                data: Object.entries(formData).map(elem => `${elem[0]}=${elem[1]}`).join('&')
+            })
+                .success(() => deferred.resolve('success'))
+                .error(() => deferred.reject('error'));
+
+            return deferred.promise;
+        }
+    }
+}]);
+
+storefrontApp.controller('newsSubscriberController', ['$scope', 'newsSubscriberService', function ($scope, newsSubscriberService) {
+    $scope.isSubscribing = false;
+
+    $scope.subscribe = function (form) {
+        $scope.isSubscribing = true;
+        if (form.$valid) {
+            newsSubscriberService.subscribe(form).then(() => $scope.isSubscribing = false, () => $scope.isSubscribing = false);
+        } else {
+            event.preventDefault();
+            $scope.isSubscribing = false;
+        }
+    };
+}]);
