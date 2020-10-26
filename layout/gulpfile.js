4933c4d42dee7412644c2ad217b4678f40d4ec0d
Sergey Podguzov <spo@virtoway.com>
2020-10-26 15:11:04 +0500

    VME-911: Bundles for AngularJS and jQuery. Gulp tasks for create bundles.

--- layout/gulpfile.js
+++ layout/gulpfile.js
@@ -7,18 +7,31 @@ var gulp = require("gulp"),
     sass = require("gulp-sass"),
     cssmin = require('gulp-cssmin'),
     rigger = require("gulp-rigger"),
-    uglify = require("gulp-uglify"),
     watch = require("gulp-watch"),
     plumber = require("gulp-plumber"),
     imagemin = require("gulp-imagemin"),
     run = require("run-sequence"),
     rimraf = require("rimraf"),
-    webserver = require("browser-sync");
+    webserver = require("browser-sync"),
+    concat = require('gulp-concat'),
+    uglifyes = require('uglify-es'),
+    composer = require('gulp-uglify/composer'),
+    uglify = composer(uglifyes, console);
 
 
 
 /* Paths to source/build/watch files
 =========================*/
+
+const scriptsPath = '../theme/assets/scripts';
+const bundlesPath = '../theme/assets/scripts/bundles';
+const angularPath = '../theme/assets/scripts/angular';
+const angularLibsPath = '../theme/assets/scripts/angular/lib';
+const angularBundlesPath = '../theme/assets/scripts/bundles/angular';
+
+function buildPaths(path, files) {
+    return files.map(file => `${path}/${file}`);
+}
 
 var path = {
     build: {
@@ -30,8 +43,50 @@ var path = {
         fonts: "build/assets/fonts/"
     },
     src: {
-        html: "src/*.{htm,html}",
-        js: "src/assets/js/*.js",
+        html: "src.{htm,html}",
+        js: {
+            angularLibs: buildPaths(angularLibsPath,
+            [
+                'angular.js',
+                'angular-resource.js',
+                'angular-route.js',
+                'angular-cookies.js',
+                'ngStorage.js',
+                'ui-bootstrap-tpls.js',
+                'angular-sanitize.js',
+                'angular-translate.js',
+                'angular-translate-loader-url.js'
+            ]),
+            angularThemeModules: buildPaths(angularPath,
+            [
+                'app.js',
+                'news.js'
+            ]),
+            angularBlogModules: buildPaths(angularPath,
+            [
+                'app.js',
+                'services.js',
+                'blog.js',
+                'news.js'
+            ]),
+            angularGlossaryModules: buildPaths(angularPath,
+            [
+                'app.js',
+                'services.js',
+                'glossary.js',
+                'news.js'
+            ]),
+            main: buildPaths(scriptsPath,
+            [
+                'jquery-3.5.0.min.js',
+                'main.js',
+                'lazysizes.min.js',
+                'jquery.validate.min.js',
+                'jquery.validate.unobtrusive.min.js',
+                'jquery.fancybox.min.js'
+            ]),
+            layout: 'src/assets/js/*.js'
+        },
         css: "src/assets/sass/main.scss",
         img: "src/assets/i/**/*.*",
         fonts: "src/assets/fonts/**/*.*"
@@ -79,16 +134,40 @@ gulp.task("css:build", function () {
         .pipe(gulp.dest(path.build.css));
 });
 
-
-gulp.task("js:build", function () {
-    return gulp.src(path.src.js)
+gulp.task('js:build-main', function () {
+    return gulp.src(path.src.js.main)
         .pipe(plumber())
         .pipe(rigger())
-        .pipe(gulp.dest(path.build.js))
+        .pipe(concat('main.min.js'))
         .pipe(uglify())
-        .pipe(removeComments())
-        .pipe(rename("main.min.js"))
-        .pipe(gulp.dest(path.build.js));
+        .pipe(gulp.dest(bundlesPath));
+});
+
+gulp.task('js:build-angular-default', function () {
+    return gulp.src(path.src.js.angularLibs.concat(path.src.js.angularThemeModules))
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(concat('angular-bundle-default.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(angularBundlesPath));
+});
+
+gulp.task('js:build-angular-blog', function () {
+    return gulp.src(path.src.js.angularLibs.concat(path.src.js.angularBlogModules))
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(concat('angular-bundle-blog.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(angularBundlesPath));
+});
+
+gulp.task('js:build-angular-glossary', function () {
+    return gulp.src(path.src.js.angularLibs.concat(path.src.js.angularGlossaryModules))
+        .pipe(plumber())
+        .pipe(rigger())
+        .pipe(concat('angular-bundle-glossary.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(angularBundlesPath));
 });
 
 
@@ -114,6 +193,7 @@ gulp.task("clean", function (cb) {
     rimraf(path.clean, cb);
 });
 
+gulp.task("js:build", gulp.series('js:build-main', 'js:build-angular-default', 'js:build-angular-blog', 'js:build-angular-glossary'));
 
 gulp.task('build', gulp.series(
         "clean",
